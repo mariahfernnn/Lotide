4f85db0941321ffb5e828c7b6d6bc7cf81e3988d
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-09-16 20:16:36 +0000

    Implement assertEqual Assignment

---
+++ lotide/assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😁😁😁 Assertion Passed: [actual] === [expected]");
+  } else {
+    console.log("👎👎👎 Assertion Failed: [actual] !== [expected]");
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Maria", "Maria");
+assertEqual("Maria", "Fernandez");
+assertEqual(3, 8);
+
+
