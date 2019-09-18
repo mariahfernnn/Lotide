8df91eb04cf2aa0f248215aec6654b46fb23f564
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-09-18 01:47:50 +0000

    assertArraysEqual function name updated

--- lotide/assertArraysEqual.js
+++ lotide/assertArraysEqual.js
@@ -9,7 +9,7 @@ const eqArrays = function(arr1, arr2) {
   }
 }
 
-const assertArrayEqual = function(actual, expected) {
+const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`😁😁😁, Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -17,7 +17,7 @@ const assertArrayEqual = function(actual, expected) {
   }
 };
 
-assertArrayEqual([1, 2, 3,], [1, 2, 3]);
-assertArrayEqual([1, 2, 3,], [3, 2, 1]);
+assertArraysEqual([1, 2, 3,], [1, 2, 3]);
+assertArraysEqual([1, 2, 3,], [3, 2, 1]);
 
 
